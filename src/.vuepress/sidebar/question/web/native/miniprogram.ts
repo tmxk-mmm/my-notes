e381504bfec0a5e8f00f1d457aaf45027a8efd52
天马行空 <2569268136@qq.com>
2024-01-19 14:24:36 +0800

    微信小程序

---
+++ src/.vuepress/sidebar/question/web/native/miniprogram.ts
@@ -0,0 +1,10 @@
+import { arraySidebar } from "vuepress-theme-hope";
+export const miniprogram = arraySidebar([
+    // "",
+    {
+        text: "微信小程序问题",
+        icon: "creative",
+        prefix: "miniprogram/",
+        children: "structure",
+    }
+])
--- src/question/web/layui/README.md
+++ src/question/web/layui/README.md
@@ -2,7 +2,7 @@
 # 当前页面内容标题
 title: layui问题
 # 当前页面图标
-icon: navigation
+icon: question
 # 分类
 category:
   - 前端问题
