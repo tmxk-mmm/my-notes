12ea5a4a9dbf71281f46496ff59f2e0fafb94031
tmxk-mmm <68832048+tmxk-mmm@users.noreply.github.com>
2024-03-05 12:37:34 +0000

    Deploying to gh-pages from @ tmxk-mmm/my-notes@5d614570b4014a79d9e261210f6512ac8e84ddc4 🚀

---
+++ assets/了解ArkTs语言.html-McECMIDy.js
@@ -0,0 +1,18 @@
+import{_ as s}from"./plugin-vue_export-helper-x3n3nnut.js";import{r,o as a,c as t,a as e,d as n,b as l,e as d}from"./app-zbZ0HQeT.js";const c={},o=e("h2",{id:"arkts语言基于typescript",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#arkts语言基于typescript","aria-hidden":"true"},"#"),n(" ArkTs语言基于TypeScript")],-1),u=e("h3",{id:"typescript基础语法",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#typescript基础语法","aria-hidden":"true"},"#"),n(" TypeScript基础语法")],-1),v={href:"https://developer.harmonyos.com/",target:"_blank",rel:"noopener noreferrer"},m=d(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// string 字符串，可用单引号双引号
+let msg: string = &quot;abc&quot;;
+// 数值，整数，浮点数都可以
+let age: number = 18;
+// boolean 布尔值，true/false
+let isMarried: boolean = true;
+// any 任意值，可以是任意类型
+let a: any = &quot;jack&quot;;
+a = 21
+// union 联合类型，可以是多种类型
+let u: string|number|boolean = &quot;rose&quot;;
+u = 18
+// Object 对象
+let p = {name:&quot;jack&quot;,age:21}
+// Array:数组，元素可以任意其他类型
+let names: Array&lt;string&gt; = [&#39;Jack&#39;,&#39;Rose&#39;];
+let ages: number[] = [18,20];
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function b(p,_){const i=r("ExternalLinkIcon");return a(),t("div",null,[o,u,e("p",null,[e("a",v,[n("TypeScript官网"),l(i)])]),m])}const f=s(c,[["render",b],["__file","了解ArkTs语言.html.vue"]]);export{f as default};
