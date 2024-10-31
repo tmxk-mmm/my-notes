17a633c6d066f126711281ae10f507c16ab9d8b6
tmxk-mmm <68832048+tmxk-mmm@users.noreply.github.com>
2024-10-31 01:36:36 +0000

    Deploying to gh-pages from @ tmxk-mmm/my-notes@209284fd46388598fad28f257fb0d72348c6530b 🚀

---
+++ assets/index.html-5d53JgmL.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as s,b as e}from"./app-hMhu8NKN.js";const a={},c=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`123456
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"})])],-1),l=[c];function o(r,i){return n(),s("div",null,l)}const m=t(a,[["render",o],["__file","index.html.vue"]]);export{m as default};
---
+++ assets/index.html-ku16nw11.js
@@ -0,0 +1,10 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,e as s}from"./app-hMhu8NKN.js";const d={},l=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>你跑得快，22岁有个家，身边全是赞叹，你跑得慢，30岁还在路上追求梦想，
+有的人为了车房拼了一辈子，有些人买辆摩托车走遍大好河山。
+你想成为怎样的人，过怎样的生活，只要你不后悔就行。
+并不是所有人都能在早上七点钟起床的，也别拿一碗饭来衡量一个人胃口的大小。
+有的人喜欢狼吞虎咽，有的人喜欢细嚼慢咽，允许别人做别人，
+也允许自己做自己，一岁有一岁的味道，跟着自己的心就好。
+不是所有选择都必须要做出正确选项的，只要你想，你可以选择你喜欢的选项。
+沿途的花会一直开，以后的路也是，祝你也祝我。
+——摘自人民日报
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),a=[l];function c(t,r){return n(),i("div",null,a)}const m=e(d,[["render",c],["__file","index.html.vue"]]);export{m as default};
